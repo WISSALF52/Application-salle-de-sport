86742799bf7a4311804d1c0271f163423ddce106
sama277 <jadlaoui.salma@gmail.com>
2025-03-28 00:54:00 +0000

    Element d'authentification Version1

--- sport/src/main/java/org/example/sport/config/SecurityConfig.java
+++ sport/src/main/java/org/example/sport/config/SecurityConfig.java
@@ -2,14 +2,54 @@ package org.example.sport.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
     }
-}
+
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf().disable()
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain uiSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .securityMatcher("/ui/**","/login/**","/logout/**")
+                .authorizeHttpRequests(ar->{
+                    ar.requestMatchers("/ui/").permitAll();
+                    ar.anyRequest().authenticated();
+                })
+                .formLogin(f->f.defaultSuccessUrl("/ui/"))
+                .build();
+    }
+
+    }
