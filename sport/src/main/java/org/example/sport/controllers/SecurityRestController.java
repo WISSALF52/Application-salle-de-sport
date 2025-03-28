1b32db9825a7da4f8468d1351702c58cb0c355ec
sama277 <jadlaoui.salma@gmail.com>
2025-03-28 00:56:24 +0000

    Element d'authentification Version2

---
+++ sport/src/main/java/org/example/sport/controllers/SecurityRestController.java
@@ -0,0 +1,14 @@
+package org.example.sport.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SecurityRestController {
+    @RequestMapping("/profile")
+    public Authentication authentication(Authentication authentication){
+        return authentication;
+    }
+}
